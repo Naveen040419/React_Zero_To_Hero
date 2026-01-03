295ac90c330ffa7f60623df1f0f796afe8019792
Naveen Adhikari <naveenadhikari@example.com>
2026-01-03 13:40:26 +0530

    "First Changes"

---
+++ Introduction/script.js
@@ -0,0 +1,29 @@
+// const heading = document.createElement("h2");
+// heading.textContent = "Hello World";
+
+// document.getElementsByClassName("root")[0].append(heading);
+
+// / ** React ** /
+
+// const reactHeading = React.createElement("h1",{ className : "react-class", id: "react-id", children: "Hello React" } );
+// console.log("React element:", reactHeading);
+
+// / ** React with JSX ** /
+
+// Updated react fragment - <> </> ....
+
+// const jsxHeading = ( 
+//     <React.Fragment>
+//     <h1 className = "jsx-heading"> Hello JSX </h1>
+//     <p className="jsx-paragraph"> Hello JSX</p>
+//     </React.Fragment>
+//     );
+    
+// console.log("JSX Heading:", jsxHeading);
+
+// // const jsxParagraph = ( <p className="jsx-paragraph"> Hello JSX</p>)
+
+// const root = ReactDOM.createRoot( document.getElementById("root"));
+
+// root.render(jsxHeading);
+// // root.render(jsxParagraph);
